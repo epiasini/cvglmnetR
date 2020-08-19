2e1d5cfc552acd817e57299e520f6ae991b71ff9
Eugenio Piasini <eugenio.piasini@gmail.com>
2020-08-19 13:33:03 -0400

    Update cvgglassoSet class name

--- cvgglassoSet.m
+++ cvgglassoSet.m
@@ -1,11 +1,11 @@
-classdef gglassoSet
+classdef cvgglassoSet
     %SGLSet Option container for gglasso
     
     properties
         loss = 'ls'
         pred_loss = 'L2'
         nfolds = 5
-        nlambda = 30 % TODO:potentially raise to 100
+        nlambda = 100
         lambda_factor = 0.001
         intercept = true
         standardize = true
